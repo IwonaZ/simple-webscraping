feff7fdd194a9955c4e759da7bed21b85cf697fa
IwonaZ <66373675+IwonaZ@users.noreply.github.com>
2022-03-24 14:03:25 +0100

    web scraping with requests_html

---
+++ script2.py
@@ -0,0 +1,35 @@
+from requests_html import HTML, HTMLSession
+import csv
+
+csv_file = open('web_scrape.csv', 'w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['headline', 'summary', 'video'])
+
+session = HTMLSession()
+r = session.get('https://coreyms.com/')
+articles = r.html.find('article')
+
+for article in articles:
+
+    headline = article.find('.entry-title-link', first=True).text
+    print(headline)
+
+    summary = article.find('.entry-content p', first=True).text
+    print(summary)
+
+    try:
+        vid_source = article.find('iframe', first=True).attrs['src']
+        vid_id = vid_source.split('/')[4]
+        vid_id = vid_id.split('?')[0]
+
+        yt_link = f'https://youtube.com/watch?v={vid_id}'
+        
+    except Exception as e:
+        yt_link = None
+
+    print(yt_link)
+    print()
+
+    csv_writer.writerow([headline, summary, yt_link])
+
+csv_file.close()
